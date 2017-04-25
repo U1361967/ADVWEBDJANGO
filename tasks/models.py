9898330f964a9578c65e852701accfbe2cba6267
Ashley Hudson <Ashley Hudson>
2017-04-25 23:31:56 +0100

    Added Superadmin User W/ Database Access

--- tasks/models.py
+++ tasks/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title + " - " + self.description
