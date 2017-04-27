937f3ce4884ec5cca96846433d061e762fe47fdf
Ashley Hudson <Ashley Hudson>
2017-04-27 10:16:51 +0100

    Register Components Added

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,11 @@
 from django.views import generic
+from django.views.generic import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse_lazy
 from .models import Task
+from .forms import UserForm
 
 class IndexView(generic.ListView):
     template_name = 'tasks/index.html'
@@ -25,3 +29,25 @@ class TaskUpdate(UpdateView):
 class TaskDelete(DeleteView):
     model = Task
     success_url = reverse_lazy('tasks:index')
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'tasks/registration_form.html'
+
+    # Display A Blank Form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name,{'form': form})
+
+    # Process Form Data
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            #Cleaned/Normalised Data
+            username = form.cleaned.data['username']
+            password = form.cleaned.data['password']
+            user.set_password(password)
+            user.save()
+            
